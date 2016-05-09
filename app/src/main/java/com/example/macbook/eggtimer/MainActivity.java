f4430ae4c55bb065980480bfe6d1dd29fb3eec40
Macbook <yzchen1994@hotmail.com>
2016-05-08 23:15:35 -0400

    Updated layout

--- app/src/main/java/com/example/macbook/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/macbook/eggtimer/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.macbook.eggtimer;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar= (SeekBar)findViewById(R.id.timerSeekBar);
+
+        final TextView timerTextView=(TextView) findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes=(int)progress/60;
+                int seconds=progress-minutes*60;
+
+                timerTextView.setText(Integer.toString(minutes)+":"+Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
